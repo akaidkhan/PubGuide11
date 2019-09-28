9eaea8c04278e658103cf65d7900dfde2839373e
Akaid <akaidkhan@gmail.com>
2019-09-28 18:09:55 +0600

    Its Latest

--- app/src/main/java/com/techneo360/pubguide11/Throwables/Throwables.java
+++ app/src/main/java/com/techneo360/pubguide11/Throwables/Throwables.java
@@ -42,11 +42,15 @@ public class Throwables extends Fragment {
 
         lstBook = new ArrayList<>();
 
-        lstBook.add(new Book("Apple","Grenade","-------------------------------------------------------------------------\n\nTemporarily removes attlefield Vision allowing for sneaky reposition"+"\n"+"\n"+"Or just a good ole plain escape.",R.drawable.apple));
+
         lstBook.add(new Book("Smoke Grenade","Grenade","-------------------------------------------------------------------------\n\nTemporarily removes attlefield Vision allowing for sneaky reposition"+"\n"+"\n"+"Or just a good ole plain escape.",R.drawable.smoke));
         lstBook.add(new Book("Frag Grenade","Grenade","-------------------------------------------------------------------------\n\nExplosive charge to fragment the outer shell of the grenade in a shaped or radial pattern."+"\n"+"\n"+"",R.drawable.grenade));
         lstBook.add(new Book("Molotov Cocktail","Grenade","-------------------------------------------------------------------------\n\nThis best use for Molotov is an AOE Denial, for example, using the fire from the Molotov on a stair case making it difficult for the enemy to cross the choke point from the burning AOE Damage."+"\n"+"\n"+"",R.drawable.firegranade));
         lstBook.add(new Book("Stun Grenade","Grenade","-------------------------------------------------------------------------\n\nWill cause strong disorientation to everyone within the blast radius (~5m) in the form of a loud ringing and a strong reduction in framerate."+"\n"+"\n"+"It should be noted that the Stun Grenade ignores cover (rocks, walls) and can affect the user, making it unsafe to use for clearing buildings.",R.drawable.standgun));
+        lstBook.add(new Book("Apple","Grenade","-------------------------------------------------------------------------\n\n"+"\n"+"\n"+"It just for throw",R.drawable.apple));
+
+
+
 
         RecyclerView myrv = (RecyclerView) v.findViewById(R.id.recyclerview_id);
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(getContext(),lstBook);
